ca7608f5632bf07c620ef8b16993d7766d1760ec
Nick Weber <webernick@icloud.com>
2019-06-11 17:02:05 -0700

    Adds Flickr Fetch request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
+// api key
+import apiKey from './config';
+
+// components
+import SearchForm from './Components/SearchForm';
+import PhotoList from './Components/PhotoList';
+
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state ={
+      photos: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=flower&per_page=12&format=json&nojsoncallback=1`)
+      .then(response => response.json())
+      .then(responseData => {
+        console.log(responseData.photos.photo);
+        this.setState({ photos: responseData.photos.photo });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
   render() {
     return (
       <div className="container">
-        <header>
           <h1>Welcome to React Gallery</h1>
-        </header>
+          <SearchForm />
+          <PhotoList />
       </div>
     );
   }
---
+++ src/Components/PhotoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PhotoList = props => {
+  return(
+    <div className="photo-container">
+      <h2>Results</h2>
+      <ul>
+        {/*comment*/}
+      </ul>
+    </div>
+  );
+}
+
+export default PhotoList;
